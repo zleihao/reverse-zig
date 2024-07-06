825770130765887ebf9a89dd0330242f3fa5e73e
zhao leihao <im_leihao@163.com>
2024-07-06 09:38:55 +0800

    使用蝶式交换法实现高效位反转

---
+++ src/reverse.zig
@@ -0,0 +1,30 @@
+const std = @import("std");
+
+///将数据依 bit 为单位进行逆序
+pub fn Reverse() type {
+    return struct {
+        const Self = @This();
+
+        pub fn reverse_byte(val: u8) u8 {
+            var x = val;
+            x = (((x & 0xaa) >> 1) | ((x & 0x55) << 1));
+            x = (((x & 0xcc) >> 2) | ((x & 0x33) << 2));
+
+            return ((x >> 4) | (x << 4));
+        }
+
+        pub fn reverse_word(val: u32) u32 {
+            var x = val;
+            // Swap adjacent bits
+            x = (((x >> 1) & 0x55555555) | ((x & 0x55555555) << 1));
+            // Swap adjacent pairs
+            x = (((x >> 2) & 0x33333333) | ((x & 0x33333333) << 2));
+            // Swap nibbles
+            x = (((x >> 4) & 0x0f0f0f0f) | ((x & 0x0f0f0f0f) << 4));
+            // Swap bytes
+            x = (((x >> 8) & 0x00ff00ff) | ((x & 0x00ff00ff) << 8));
+            // Swap 16-bit halves
+            return ((x >> 16) | (x << 16));
+        }
+    };
+}
